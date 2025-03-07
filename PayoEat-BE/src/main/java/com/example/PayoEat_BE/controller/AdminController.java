c9ba7017bad2b566496d3d7946719f9c9f1fcaf3
abedsully <abedsully2909@gmail.com>
2025-03-07 07:33:04 +0700

    feat: added admin service with controller

--- PayoEat-BE/src/main/java/com/example/PayoEat_BE/controller/AdminController.java
+++ PayoEat-BE/src/main/java/com/example/PayoEat_BE/controller/AdminController.java
@@ -1,4 +1,41 @@
 package com.example.PayoEat_BE.controller;
 
+import com.example.PayoEat_BE.model.RestaurantApproval;
+import com.example.PayoEat_BE.response.ApiResponse;
+import com.example.PayoEat_BE.service.admin.IAdminService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+@Tag(name = "Admin Controller", description = "Endpoint for managing admin service")
 public class AdminController {
+    private final IAdminService adminService;
+
+    @PostMapping("/approve-restaurant/{id}")
+    public ResponseEntity<ApiResponse> approveRestaurant(@PathVariable UUID id) {
+        try {
+            adminService.approveRestaurant(id);
+            return ResponseEntity.ok(new ApiResponse("Restaurant is approved", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error: " + e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/reject-restaurant/{id}")
+    public ResponseEntity<ApiResponse> approveRestaurant(@PathVariable UUID id, @RequestParam String rejectionMessage) {
+        try {
+            adminService.rejectRestaurant(id, rejectionMessage);
+            return ResponseEntity.ok(new ApiResponse("Restaurant is rejected", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error: " + e.getMessage(), null));
+        }
+    }
 }
--- PayoEat-BE/src/main/java/com/example/PayoEat_BE/service/admin/IAdminService.java
+++ PayoEat-BE/src/main/java/com/example/PayoEat_BE/service/admin/IAdminService.java
@@ -3,8 +3,10 @@ package com.example.PayoEat_BE.service.admin;
 import com.example.PayoEat_BE.model.RestaurantApproval;
 import com.example.PayoEat_BE.request.restaurant.ReviewRestaurantRequest;
 
+import java.util.UUID;
+
 public interface IAdminService {
     RestaurantApproval reviewRestaurant(ReviewRestaurantRequest request);
-    void approveRestaurant(RestaurantApproval approvalRequest);
-    void rejectRestaurant(RestaurantApproval approvalRequest, String rejectionMessage);
+    void approveRestaurant(UUID id);
+    void rejectRestaurant(UUID id, String rejectionMessage);
 }
