3fefad1d440887fa3249aa1917993a59c42bbcb4
abedsully <abedsully2909@gmail.com>
2025-06-02 14:11:30 +0700

    deletion: remove unnecessary file

--- PayoEat-BE/src/main/java/com/example/PayoEat_BE/repository/BalanceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.PayoEat_BE.repository;
-
-import com.example.PayoEat_BE.model.Balance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BalanceRepository extends JpaRepository<Balance, Long> {
-    Balance findByUserId(Long userId);
-}
