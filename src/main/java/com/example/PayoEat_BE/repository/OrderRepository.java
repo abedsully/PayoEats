2d44fb6850fba567ea6117a1f93a900bb7b75f5f
abedsully <abedsully2909@gmail.com>
2026-01-26 09:26:39 +0700

    feat: push

--- src/main/java/com/example/PayoEat_BE/repository/OrderRepository.java
+++ src/main/java/com/example/PayoEat_BE/repository/OrderRepository.java
@@ -663,7 +663,8 @@ public class OrderRepository {
                 m.menu_name,
                 m.menu_price,
                 m.menu_image_url,
-                oi.quantity
+                oi.quantity,
+                (oi.quantity * m.menu_price) AS menu_total_price
             FROM orders o
             JOIN restaurant r ON r.id = o.restaurant_id
             JOIN order_items oi ON oi.order_id = o.id
